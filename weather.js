82eb46294bf2efaf16736eab5529d11d8120dada
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-09-06 01:59:00 -0500

    Remove superflous comment and data
    I also want to see what kind of warning I get.

--- weather.js
+++ weather.js
@@ -3,57 +3,6 @@ const searchButton = document.querySelector('#search-button');
 const mainElement = document.querySelector('main');
 searchElement.focus();
 
-const kelvinToFahrenheit = (k) => (((k - 273.15) * 9)/5)+32;
-
-const data = {
-  coord: { lon: -0.1257, lat: 51.5085 },
-  weather: [
-    { id: 804, main: "Clouds", description: "overcast clouds", icon: "04d" },
-  ],
-  base: "stations",
-  main: {
-    temp: 288.84,
-    feels_like: 288.89,
-    temp_min: 286.14,
-    temp_max: 291.01,
-    pressure: 1014,
-    humidity: 93,
-  },
-  visibility: 10000,
-  wind: { speed: 2.57, deg: 230 },
-  clouds: { all: 100 },
-  dt: 1662355523,
-  sys: {
-    type: 2,
-    id: 2075535,
-    country: "GB",
-    sunrise: 1662355162,
-    sunset: 1662403175,
-  },
-  timezone: 3600,
-  id: 2643743,
-  name: "London",
-  cod: 200,
-};
-
-{
-/* Create This:
-<div class="weather-card">
-    <div class="forecast">
-        <div class="overview">
-            <div class="current-weather no-pad">Current Weather</div>
-            <div class="city-name no-pad">Berrien Springs</div>        
-        </div>
-        <div class="temperature farenheit">69</div>
-        <div class="conditions">
-            <p class="farenheit">FEELS LIKE: 70</p>
-            <p>WIND: 5 MPH</p>
-            <p>HUMIDITY: 69%</p>
-        </div>    
-    </div>
-</div>
-*/
-}
 function addForecast(data) {
     console.log(data);
 
@@ -114,7 +63,7 @@ const removeForecastFromPage = () => {
 
 async function getForecast() {
     try {
-        const apiKey = 'APIKEYHERE';
+        const apiKey = 'e98806a61a3fd4d2482ec210f64bd243';
         const city = searchElement.value;
         const httpResponse = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=${apiKey}`);
         const data = await httpResponse.json();
